741181473be9f55197c40eff9f94f8f03d6ac13f
Eias <51035404+1Eias@users.noreply.github.com>
2025-01-21 12:21:19 +0100

    Stap 2: Berekening van de graaf- en afvoerkosten

--- Callenge 2 - Zwembadofferte-tool.py
+++ Callenge 2 - Zwembadofferte-tool.py
@@ -4,4 +4,10 @@ depth = 1.5
 
 volume = round(length * width * depth, 2)
 
-print(volume)
+excavation_cost = volume * 25
+soil_removal_cost = volume * 32.50
+
+print(f"Offerte voor een zwembad van {length} bij {width} bij {depth} meter (inhoud: {volume} m3)")
+print(f"Uitgraven:                         € {excavation_cost:.2f}")
+print(f"Afvoeren grond:              € {soil_removal_cost:.2f}")
+print(f"Totaal:                               € {excavation_cost + soil_removal_cost:.2f}")
