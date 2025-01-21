12226056ac24a450bec729832ba08372cadfe121
Eias <51035404+1Eias@users.noreply.github.com>
2025-01-21 12:23:22 +0100

    Stap 3: Bereken voorrijkosten

--- Callenge 2 - Zwembadofferte-tool.py
+++ Callenge 2 - Zwembadofferte-tool.py
@@ -7,7 +7,22 @@ volume = round(length * width * depth, 2)
 excavation_cost = volume * 25
 soil_removal_cost = volume * 32.50
 
+distance = 60
+
+if volume < 20:
+    fixed_price = 100
+else:
+    fixed_price = 250
+
+if distance < 50:
+    distance_cost = distance * 1.25
+else:
+    distance_cost = distance * 2.05
+
+transport_cost = fixed_price + distance_cost
+
 print(f"Offerte voor een zwembad van {length} bij {width} bij {depth} meter (inhoud: {volume} m3)")
 print(f"Uitgraven:                         € {excavation_cost:.2f}")
 print(f"Afvoeren grond:              € {soil_removal_cost:.2f}")
-print(f"Totaal:                               € {excavation_cost + soil_removal_cost:.2f}")
+print(f"Voorrijkosten                    € {transport_cost:.2f}")
+print(f"Totaal:                               € {excavation_cost + soil_removal_cost + transport_cost:.2f}")
